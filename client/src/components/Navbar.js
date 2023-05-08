19061d3bbd6f977b27417b305e3c9eb827bef976
Jassi10000 <jaskeerat10000@gmail.com>
2023-05-08 22:33:32 +0530

    fix: main resolved some server starting issues , also did a basic layout of navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { navLinks } from '../constants';
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
+  const [ active , setActive ] = useState("");
+
   return (
-    <div>Navbar</div>
+    <nav>
+      <div style={{ display: 'flex'}}>
+        <Link to='/' onClick={() => {
+          setActive("");
+          window.scrollTo(0,0);
+        }}>
+            <img src={logo} height="100px" width="100px"/>
+        </Link>
+        <ul style={{ display: 'flex'}}>
+          {
+            navLinks.map((link) => {
+              return <li key={link.id} onClick={() => setActive(link.title)}style={{
+                marginRight: '30px'
+              }}>
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            })
+          }
+        </ul>
+      </div> 
+    </nav>
   )
 }
 
